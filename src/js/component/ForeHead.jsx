92cad18f040eef4af39fba9382a46fac152d03b2
Jose Pindado <joseangelpindado@gmail.com>
2024-10-14 19:15:13 +0000

    First commit

--- src/js/component/ForeHead.jsx
+++ src/js/component/ForeHead.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ForeHead = () => {
   return (
     <div className="container py-5 bg-body-secondary rounded" style={{marginTop: '80px' }}>
-      <h1 className="display-5 fw-bold">My first React Project</h1>
+      <h1 className="display-5 fw-bold">My first React project</h1>
       <p className="col-md-8 fs-4">
       Lorem ipsum dolor sit amet consectetur adipiscing elit pretium, sapien per sociosqu platea imperdiet mattis vestibulum nascetur, ullamcorper pharetra bibendum a et ultricies rutrum. Suscipit congue ligula urna eros aenean tempus class senectus, fusce imperdiet velit sodales iaculis mus proin ullamcorper, hendrerit leo penatibus praesent cras dis commodo.</p>
       <button className="btn btn-primary btn-lg" type="button">
