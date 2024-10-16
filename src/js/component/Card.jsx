4a2ce2999e458562abacc489314c275c3282b41f
Jose Pindado <joseangelpindado@gmail.com>
2024-10-16 18:41:49 +0000

    Third commit

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Card = ({title, description, images}) => {
   return (
-    <div className="card my-3 mx-auto" style= {{ width: "18rem", boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)" }}>
+    <div className="card my-3 mx-auto" style= {{ width: "18rem"}}>
     <img src={images} className="card-img-top" alt="..." style={{ height: "200px", objectFit: "cover", borderRadius: "8px 8px 0 0"}}/>
     <div className="card-body">
       <h3 className="card-title">{title}</h3>
--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -9,9 +9,27 @@ const Navbar = () => {
         </a>
         <button
           className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse">
+          type="button" 
+          data-bs-toggle="collapse" 
+          data-bs-target="#navbarTogglerDemo01" 
+          aria-controls="navbarTogglerDemo01" 
+          aria-expanded="false" 
+          aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
         </button>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <ul className="navbar-nav text-white me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+               <a className="nav-link active text-end mt-3 me-3" href="#">Item 1</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active text-end me-3" href="#">Item 2</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active text-end me-3" href="#">Item 3</a>
+             </li>
+            </ul>
+        </div>
       </div>
     </nav>
   );
